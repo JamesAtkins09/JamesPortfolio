cb1456344cf89682c023d14feebbd80b95e5af4e
jamesatkins09 <jamesatkins1537@gmail.com>
2024-01-14 19:26:35 +0845

    changed config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig =     The "next export" command has been removed in favor of "output: export" in next.config.js. Learn more: https://nextjs.org/docs/advanced-features/static-html-export
+
 
 module.exports = nextConfig
