857d883520173a5d3ee6bdb5e1c6a6175cb25eaa
jamesatkins09 <jamesatkins1537@gmail.com>
2024-01-14 19:32:24 +0845

    check

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig =     The "next export" command has been removed in favor of "output: export" in next.config.js. Learn more: https://nextjs.org/docs/advanced-features/static-html-export
-
+const nextConfig = {
+    exportPathMap: () => ({
+        '/': { page: '/' }
+    })
+}
 
 module.exports = nextConfig
